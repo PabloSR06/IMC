d3bbef5f69a1dae5d502e691cb778a11460a2614
PabloSR06 <47283527+PabloSR06@users.noreply.github.com>
2021-09-30 17:41:39 +0100

    Ventana vacia

--- src/main/java/dad/imc/IMC.java
+++ src/main/java/dad/imc/IMC.java
@@ -1,21 +1,42 @@
 package dad.imc;
 
-import java.awt.Label;
-import java.awt.TextField;
-
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class IMC extends Application {
 	
 	private TextField pesoText;
 	private TextField alturaText;
+	
+	private Label pesoLabel;
+	private Label alturaLabel;
+	
+	
 	private Label imcLabel;
 	private Label textLabel;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
+		pesoText = new TextField();
+		alturaText = new TextField();
+		pesoLabel = new Label("x");
+		alturaLabel = new Label("x");
+		
+		HBox root = new HBox(5, pesoLabel ,pesoText);
+		root.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(root, 320, 200);
+		
+		primaryStage.setTitle("IMC");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 		
 	}
 
