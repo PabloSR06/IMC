72fa0dc14879703239a69bb28c091ae7aa01865e
PabloSR06 <47283527+PabloSR06@users.noreply.github.com>
2021-09-30 17:28:07 +0100

    first commit

---
+++ src/main/java/dad/imc/IMC.java
@@ -0,0 +1,26 @@
+package dad.imc;
+
+import java.awt.Label;
+import java.awt.TextField;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class IMC extends Application {
+	
+	private TextField pesoText;
+	private TextField alturaText;
+	private Label imcLabel;
+	private Label textLabel;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ src/main/java/dad/imc/Main.java
@@ -0,0 +1,9 @@
+package dad.imc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		IMC.main(args);
+	}
+
+}
